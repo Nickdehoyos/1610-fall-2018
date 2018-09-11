c1ea1d8b60bdfe5116465f293c8f053f0f28ed0b
Nick de Hoyos <nickdehoyos2@gmail.com>
2018-09-11 14:47:05 -0600

    Practicing C# classes

---
+++ New Unity Project/Assets/Husky.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Husky : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake ()
+	{
+		print("I am a Husky.");	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

ce2815ea8c01f6bdc917bceeacf4895878119493
Nick de Hoyos <nickdehoyos2@gmail.com>
2018-09-17 15:06:19 -0600

    Move practice and camera look at

--- New Unity Project/Assets/Move.cs
+++ New Unity Project/Assets/Move.cs
@@ -9,21 +9,23 @@ public class Move : MonoBehaviour {
 		
 	}
 
-	public CharacterController Character;
 	public float Speed;
-	public Vector3 MoveSpeed;
-    public Vector3 RotateSpeed;
+	public float MoveSpeed = 10f;
+    public float TurnSpeed = 50f;
+		
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		RotateSpeed.y = Speed * Input.GetAxis("Vertical");
-		RotateSpeed *= Time.deltaTime;
+		if (Input.GetKey(KeyCode.UpArrow))
+			transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+		if (Input.GetKey(KeyCode.DownArrow))
+			transform.Translate(-Vector3.forward * MoveSpeed * Time.deltaTime);
+		if (Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(-Vector3.up, TurnSpeed * Time.deltaTime);
+		if (Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.up, TurnSpeed * Time.deltaTime);
+			
 		
-		MoveSpeed.x = Speed*Input.GetAxis("Horizontal");
-		MoveSpeed *= Time.deltaTime;
-
-		Character.Move(MoveSpeed);
-		
-	}
+	}		
 }

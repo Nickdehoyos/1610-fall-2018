93edd42bd35f0665a0ea1af85bafc518c8bdd497
Nick de Hoyos <nickdehoyos2@gmail.com>
2018-10-03 11:15:26 -0600

    If statement practice

---
+++ New Unity Project/Assets/IfPowerUpMax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+	
+public class IfPowerUpMax : MonoBehaviour
+{
+	public int A, B;
+
+
+	void Update()
+	{
+
+		if (A >= B)
+	
+			{
+				print("Wizard powered up!");
+			}
+		
+
+	}
+}	

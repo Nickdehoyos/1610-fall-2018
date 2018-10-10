0f961755d399151dbc5e15379f0c98382153a440
Nick de Hoyos <nickdehoyos2@gmail.com>
2018-10-10 15:34:53 -0600

    WhoCanSeeIt

---
+++ New Unity Project/Assets/Scripts/WhatCanSeeIt.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCanSeeIt: ScriptableObject
+{
+
+    private float health;
+    protected float PowerLevel;
+    public float EnergyLevel;
+
+    private void AddHealth()
+    {
+        health += 100;
+    }
+
+    protected void AddPower()
+    {
+        AddHealth();
+    }
+
+    public void AddEnergy()
+    {
+        
+    }
+    
+}
+
+public class ICanSeeIt: MonoBehaviour
+{
+    private WhatCanSeeIt NewObject;
+
+    private void Start()
+    {
+        NewObject.EnergyLevel = 100;
+        NewObject.AddEnergy();
+    }
+}
+
+public class Child : WhatCanSeeIt
+{
+    void CallThings()
+    {
+        PowerLevel = 200;
+        EnergyLevel = 300;
+        AddPower();
+        AddEnergy();
+    }
+}
